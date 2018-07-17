1d57589f000eb1a286f68dd9c2f6166b3a15fb29
YinYichang <yichang1776@163.com>
2018-07-17 15:01:53 +0800

    Initial commit

---
+++ src/com/scalademo/HelloWorld.scala
@@ -0,0 +1,41 @@
+package com.scalademo
+
+object HelloWorld {
+
+  class HelloWorld {
+    def test1(): Unit = {
+      println("Hello black kids!")
+    }
+  }
+
+  def test(): Unit = {
+    println("Hello White kids!")
+  }
+
+  def arr(): Unit = {
+    val arr = Array("a", "b", "c")
+
+    arr.iterator.foreach(obj => {
+      print(obj + "  ")
+    })
+  }
+
+  def multi(): Unit = {
+    println("九九乘法口诀：")
+    for (i <- 1 to 9) {
+      for (j <- 1 to i) {
+        print(i + "*" + j + "=" + i * j + "  ")
+      }
+      println()
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    test()
+    arr()
+    multi()
+    var helloWorld = new HelloWorld()
+    helloWorld.test1()
+    println("Hello World!")
+  }
+}
---
+++ src/com/scalademo/Test.java
@@ -0,0 +1,8 @@
+package com.scalademo;
+
+public class Test {
+    public static void main(String[] args) {
+        HelloWorld.HelloWorld hiWorld = new HelloWorld.HelloWorld();
+        hiWorld.test1();
+    }
+}
