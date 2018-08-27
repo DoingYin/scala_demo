93007713809ff18a7c5f6e6d17e178f118241306
YinYichang <yichang1776@163.com>
2018-08-27 15:48:04 +0800

    scala学习之对象继承

---
+++ src/com/scalademo/extend_demo.scala
@@ -0,0 +1,33 @@
+package com.scalademo
+
+object extend_demo {
+
+}
+
+class Person (var name:String) {
+  private var age = 18
+}
+
+//继承于Person，主构造函数继承Person的name属性
+class Student(var no:Int, name: String) extends Person(name:String) {
+  private val score = 60.0
+}
+
+object testExetend {
+  def main(args: Array[String]): Unit = {
+    val p:Person = new Student
+    var s:Student = null
+    if (p.isInstanceOf[Student]) {
+      s = p.asInstanceOf[Student]
+    }
+
+   /* val s1:Student = new Person
+    var p1:Person = null
+    if (s1.isInstanceOf[Person]) {
+      p1 = s1.asInstanceOf[Student]
+    }*/
+
+    println(p.getClass == classOf[Person])
+    println(s.getClass == classOf[Student])
+  }
+}
